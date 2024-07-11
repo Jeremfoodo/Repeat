dd728a265a4d89b65e4b3266ae0e841737167df5
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-07-11 13:11:36 +0200

    Update account_analysis.py

--- account_analysis.py
+++ account_analysis.py
@@ -4,6 +4,7 @@ from src.data_processing import filter_data_by_account
 from src.calculations import calculate_segments_for_month
 from src.plots import plot_ratios
 from global_analysis import generate_summary_boxes
+from st_aggrid import AgGrid, GridOptionsBuilder, JsCode
 
 @st.cache_data
 def get_filtered_data(df, account_manager):
@@ -56,3 +57,50 @@ def account_analysis(df):
     for segment in ['Nouveaux Clients', 'Clients Récents', 'Anciens Clients']:
         fig = plot_ratios(segment, account_results, account_manager)
         st.plotly_chart(fig)
+
+    # Préparer le tableau
+    df['Derniere commande'] = df.groupby('Restaurant ID')['Date de commande'].transform('max')
+    df_latest = df[df['Owner email'] == account_manager].drop_duplicates('Restaurant ID')
+
+    def client_type(row):
+        if row['Mois'] == '2024-07':
+            return 'Acquisition'
+        elif row['Mois'] == '2024-06':
+            return 'Nouveaux Clients'
+        elif row['Mois'] in ['2024-03', '2024-04', '2024-05']:
+            return 'Clients Récents'
+        else:
+            return 'Anciens Clients'
+
+    df_latest['Type de Client'] = df_latest.apply(client_type, axis=1)
+
+    # Mise en forme des données pour le tableau
+    df_display = df_latest[['Owner email', 'Restaurant ID', 'Restaurant', 'Type de Client', 'Derniere commande']]
+
+    # Colorer les lignes en fonction de la date de dernière commande
+    js = JsCode("""
+    function(params) {
+        if (params.data['Derniere commande'].startsWith('2024-07')) {
+            return {
+                'color': 'white',
+                'backgroundColor': 'green'
+            }
+        } else {
+            return {
+                'color': 'white',
+                'backgroundColor': 'red'
+            }
+        }
+    }
+    """)
+
+    gb = GridOptionsBuilder.from_dataframe(df_display)
+    gb.configure_columns(df_display.columns, filterable=True, sortable=True, resizable=True)
+    gb.configure_column("Derniere commande", cellStyle=js)
+    gb.configure_default_column(groupable=True)
+    gb.configure_grid_options(domLayout='normal')
+
+    grid_options = gb.build()
+
+    st.header(f'Détails des Clients - {account_manager}')
+    AgGrid(df_display, gridOptions=grid_options, enable_enterprise_modules=True, fit_columns_on_grid_load=True, allow_unsafe_jscode=True)
