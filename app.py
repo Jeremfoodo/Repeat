37dbeab618550dee54b276eddd07505ce8633497
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-07-11 12:47:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import streamlit as st
-import pandas as pd
 from streamlit_option_menu import option_menu
-from src.data_processing import load_data, download_files, filter_data_by_account
-from src.calculations import process_country_data, calculate_segments_for_month
-from src.plots import plot_ratios
+from src.data_processing import load_data, download_files
+from global_analysis import global_analysis
+from account_analysis import account_analysis
 
 # Fonction pour vider le cache
 def clear_cache():
@@ -15,30 +14,6 @@ def clear_cache():
 def get_data():
     download_files()
     return load_data()
-
-# Fonction pour générer des boîtes de résumé
-def generate_summary_boxes(june_2024_results):
-    colors = {
-        'Acquisition': '#FFCCCC',
-        'Nouveaux Clients': '#CCFFCC',
-        'Clients Récents': '#CCCCFF',
-        'Anciens Clients': '#FFCC99'
-    }
-
-    boxes = []
-    for segment in ['Acquisition', 'Nouveaux Clients', 'Clients Récents', 'Anciens Clients']:
-        segment_data = june_2024_results[june_2024_results['Segment'] == segment].iloc[0]
-        box = f"""
-        <div style="background-color: {colors[segment]}; padding: 10px; margin: 10px; border-radius: 5px; width: 90%; height: 170px;">
-            <h4 style="margin: 0; font-size: 16px;">{segment}</h4>
-            <p style="margin: 2px 0; font-size: 14px;">Nombre de Clients: {segment_data['Nombre de Clients']}</p>
-            <p style="margin: 2px 0; font-size: 14px;">Nombre de Clients Possible: {segment_data['Nombre de Clients Possible']}</p>
-            <p style="margin: 2px 0; font-size: 14px;">Nombre de Clients Actifs (Mois Précédent): {segment_data['Nombre de Clients Actifs (Mois Précédent)']}</p>
-            <p style="margin: 2px 0; font-size: 14px;">Rapport (%): {segment_data['Rapport (%)']}</p>
-        </div>
-        """
-        boxes.append(box)
-    return boxes
 
 # Interface utilisateur avec Streamlit
 st.set_page_config(page_title="Analyse de la Rétention", layout="wide")
@@ -57,72 +32,7 @@ selected = option_menu(
 historical_data, df = get_data()
 
 if selected == "Analyse Globale":
-    st.title('Analyse de la Rétention des Clients')
-
-    # Bouton pour mettre à jour les données
-    if st.button('Mettre à jour'):
-        clear_cache()
-
-    country_code = st.selectbox('Sélectionner un pays', list(historical_data.keys()) + ['Global'])
-
-    if country_code == 'Global':
-        all_historical_data = pd.concat(historical_data.values(), ignore_index=True)
-        recent_months = pd.date_range(start='2024-05-01', end='2024-07-01', freq='MS').strftime('%Y-%m').tolist()
-        recent_results = pd.concat([calculate_segments_for_month(df, month) for month in recent_months], ignore_index=True)
-        all_results = pd.concat([all_historical_data, recent_results], ignore_index=True)
-    else:
-        all_results = process_country_data(df, historical_data, country_code)
-
-    june_2024_results = all_results[all_results['Mois'] == '2024-07']
-
-    st.header('Résumé des Segments pour Juillet 2024')
-    summary_boxes = generate_summary_boxes(june_2024_results)
-
-    # Afficher les boîtes dans une grille 2x2
-    col1, col2 = st.columns(2)
-    with col1:
-        st.markdown(summary_boxes[0], unsafe_allow_html=True)
-        st.markdown(summary_boxes[1], unsafe_allow_html=True)
-    with col2:
-        st.markdown(summary_boxes[2], unsafe_allow_html=True)
-        st.markdown(summary_boxes[3], unsafe_allow_html=True)
-
-    st.header('Graphiques des Segments')
-    for segment in ['Nouveaux Clients', 'Clients Récents', 'Anciens Clients']:
-        fig = plot_ratios(segment, all_results, country_code)
-        st.plotly_chart(fig)
+    global_analysis(historical_data, df)
 
 elif selected == "Par Account":
-    st.title('Analyse de la Rétention par Account Manager')
-
-    # Sélection de l'account manager
-    account_manager = st.selectbox(
-        'Sélectionner un account manager',
-        sorted(df['Owner email'].unique())
-    )
-
-    # Filtrer les données par account manager
-    df_account = filter_data_by_account(df, account_manager)
-
-    # Calculer les segments pour chaque mois depuis avril 2024 pour l'account manager
-    recent_months = pd.date_range(start='2024-04-01', end='2024-07-01', freq='MS').strftime('%Y-%m').tolist()
-    account_results = pd.concat([calculate_segments_for_month(df_account, month) for month in recent_months], ignore_index=True)
-
-    june_2024_results_account = account_results[account_results['Mois'] == '2024-07']
-
-    st.header(f'Résumé des Segments pour Juillet 2024 - {account_manager}')
-    summary_boxes_account = generate_summary_boxes(june_2024_results_account)
-
-    # Afficher les boîtes dans une grille 2x2
-    col1, col2 = st.columns(2)
-    with col1:
-        st.markdown(summary_boxes_account[0], unsafe_allow_html=True)
-        st.markdown(summary_boxes_account[1], unsafe_allow_html=True)
-    with col2:
-        st.markdown(summary_boxes_account[2], unsafe_allow_html=True)
-        st.markdown(summary_boxes_account[3], unsafe_allow_html=True)
-
-    st.header(f'Graphiques des Segments - {account_manager}')
-    for segment in ['Nouveaux Clients', 'Clients Récents', 'Anciens Clients']:
-        fig = plot_ratios(segment, account_results, account_manager)
-        st.plotly_chart(fig)
+    account_analysis(df)
