e448fabffc75995c76c5308b18c3bf7129176ee0
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-07-11 12:52:03 +0200

    Update account_analysis.py

--- account_analysis.py
+++ account_analysis.py
@@ -5,6 +5,14 @@ from src.calculations import calculate_segments_for_month
 from src.plots import plot_ratios
 from global_analysis import generate_summary_boxes
 
+@st.cache(allow_output_mutation=True)
+def get_filtered_data(df, account_manager):
+    return filter_data_by_account(df, account_manager)
+
+@st.cache(allow_output_mutation=True)
+def get_account_results(df_account, recent_months):
+    return pd.concat([calculate_segments_for_month(df_account, month) for month in recent_months], ignore_index=True)
+
 def account_analysis(df):
     st.title('Analyse de la Rétention par Account Manager')
 
@@ -17,16 +25,22 @@ def account_analysis(df):
         sorted(df['Owner email'].unique())
     )
 
+    # Bouton pour mettre à jour les données
+    if st.button('Mettre à jour'):
+        st.caching.clear_cache()
+        st.experimental_rerun()
+
     # Filtrer les données par account manager
-    df_account = filter_data_by_account(df, account_manager)
+    df_account = get_filtered_data(df, account_manager)
 
     # Calculer les segments pour chaque mois depuis avril 2024 pour l'account manager
     recent_months = pd.date_range(start='2024-04-01', end='2024-07-01', freq='MS').strftime('%Y-%m').tolist()
-    account_results = pd.concat([calculate_segments_for_month(df_account, month) for month in recent_months], ignore_index=True)
+    account_results = get_account_results(df_account, recent_months)
 
     june_2024_results_account = account_results[account_results['Mois'] == '2024-07']
 
-    st.header(f'Résumé des Segments pour Juillet 2024 - {account_manager}')
+    st.header(f'Résumé des Segments pour Juillet 2024')
+    st.subheader(f'<span style="font-size:14px; color:black; text-decoration:none;">{account_manager}</span>', unsafe_allow_html=True)
     summary_boxes_account = generate_summary_boxes(june_2024_results_account)
 
     # Afficher les boîtes dans une grille 2x2
