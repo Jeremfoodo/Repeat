19dd95347ef92790162b32c9513800681a54352a
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-07-11 13:24:11 +0200

    Create objectifs.py

---
+++ objectifs.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def objectifs_page():
+    st.title('Définir les Objectifs des Account Managers')
+    st.write("Cette page permettra aux managers de définir et de suivre les objectifs des account managers.")
+    # Ajoutez ici les éléments de la page objectifs
