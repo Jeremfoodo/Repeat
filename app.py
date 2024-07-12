be88652e0c4d75484de69e5bb1cb7e4e7eadd22b
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-07-12 14:18:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,42 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
+from src.data_processing import load_data, download_files, reassign_account_manager
 from global_analysis import global_analysis
 from account_analysis import account_analysis
 from objectifs import objectifs_page
-from data_processing import load_historical_data, load_recent_data, preprocess_data
 
 # Fonction pour vider le cache
 def clear_cache():
     st.cache_data.clear()
+    st.experimental_rerun()
 
-# Charger les données historiques et récentes
-historical_data = load_historical_data()
-df_recent = load_recent_data()
-df_recent = preprocess_data(df_recent)
+# Télécharger et charger les données avec mise en cache
+@st.cache_data
+def get_data():
+    download_files()
+    historical_data, df = load_data()
+    df = reassign_account_manager(df)
+    return historical_data, df
 
-# Barre de navigation
-st.sidebar.title("Navigation")
-selected = st.sidebar.radio("Go to", ["Analyse Globale", "Par Account", "Objectifs"])
+# Interface utilisateur avec Streamlit
+st.set_page_config(page_title="Analyse de la Rétention", layout="wide")
 
-# Pages
+# Menu de navigation
+selected = option_menu(
+    menu_title=None,
+    options=["Analyse Globale", "Par Account", "Objectifs"],
+    icons=["bar-chart", "person-circle", "bullseye"],
+    menu_icon="cast",
+    default_index=0,
+    orientation="horizontal"
+)
+
+# Télécharger et charger les données
+historical_data, df = get_data()
+
 if selected == "Analyse Globale":
-    global_analysis(historical_data, df_recent)
+    global_analysis(historical_data, df)
 elif selected == "Par Account":
-    account_analysis(df_recent)
+    account_analysis(df)
 elif selected == "Objectifs":
     objectifs_page()
-
-# Bouton pour mettre à jour les données
-if st.sidebar.button('Mettre à jour'):
-    clear_cache()
-    st.experimental_rerun()
