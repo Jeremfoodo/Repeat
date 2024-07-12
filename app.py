9dfde61155518a4643e863fac516d05ed25f5cfa
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-07-12 14:16:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ import streamlit as st
 from global_analysis import global_analysis
 from account_analysis import account_analysis
 from objectifs import objectifs_page
+from data_processing import load_historical_data, load_recent_data, preprocess_data
 
 # Fonction pour vider le cache
 def clear_cache():
     st.cache_data.clear()
+
+# Charger les données historiques et récentes
+historical_data = load_historical_data()
+df_recent = load_recent_data()
+df_recent = preprocess_data(df_recent)
 
 # Barre de navigation
 st.sidebar.title("Navigation")
@@ -13,9 +19,9 @@ selected = st.sidebar.radio("Go to", ["Analyse Globale", "Par Account", "Objecti
 
 # Pages
 if selected == "Analyse Globale":
-    global_analysis()
+    global_analysis(historical_data, df_recent)
 elif selected == "Par Account":
-    account_analysis()
+    account_analysis(df_recent)
 elif selected == "Objectifs":
     objectifs_page()
 
