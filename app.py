616dc6c8dbcd1e11eea15460d769077515ba76d0
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-07-22 14:27:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from global_analysis import global_analysis
 from account_analysis import account_analysis
 from objectifs import objectifs_page
 from active_users import active_users_page
-from src.data_processing import load_data, download_files, reassign_account_manager
+from src.data_processing import load_data, download_prepared_data, reassign_account_manager
 
 # Télécharger et charger les données
-download_files()
+download_prepared_data()
 historical_data, df = load_data()
 
 # Menu vertical
