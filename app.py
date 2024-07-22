d4bfea67da4e150393e6b118844d15161f80f13f
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-07-22 18:17:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,27 +4,23 @@ from global_analysis import global_analysis
 from account_analysis import account_analysis
 from objectifs import objectifs_page
 from active_users import active_users_page
-from src.data_processing import load_data, download_prepared_data, reassign_account_manager
-from src.database import init_db
+from src.data_processing import load_data, download_prepared_data, reassign_account
 
-# Initialiser la base de données
-init_db()
-
-
-# Télécharger le fichier volumineux et charger les données
+# Télécharger et charger les données
 download_prepared_data()
 historical_data, df = load_data()
 
 # Menu vertical
-selected = option_menu(
-    "Menu",
-    ["Analyse Globale", "Par Account", "Objectifs", "Active Users"],
-    icons=["graph-up", "person", "bullseye", "bar-chart"],
-    menu_icon="cast",
-    default_index=0,
-    orientation="vertical",
-)
+with st.sidebar:
+    selected = option_menu(
+        "Menu",
+        ["Analyse Globale", "Par Account", "Objectifs", "Active Users"],
+        icons=["bar-chart", "person-circle", "target", "graph-up"],
+        menu_icon="cast",
+        default_index=0,
+    )
 
+# Afficher la page sélectionnée
 if selected == "Analyse Globale":
     global_analysis(historical_data, df)
 elif selected == "Par Account":
