0451b529cc6da3eb0fe3c962c391b7d2e4dc4b49
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-07-22 10:42:17 +0200

    Update data_processing.py

--- src/data_processing.py
+++ src/data_processing.py
@@ -4,7 +4,7 @@ import os
 
 # URLs de Google Drive pour chaque fichier
 gdrive_files = {
-        'FR': 'https://drive.google.com/uc?id=1HSagRx3aiT3Jb9idOYtdINlYfp4SsqUE',
+    'FR': 'https://drive.google.com/uc?id=1HSagRx3aiT3Jb9idOYtdINlYfp4SsqUE',
     'US': 'https://drive.google.com/uc?id=1Ls5d_1G9E3XeiktLzZs6MXTxGzRv7jTb',
     'BE': 'https://drive.google.com/uc?id=1pLdrmiP715kfG_7ToVhXKXqp5lo8-X48',
     'GB': 'https://drive.google.com/uc?id=1j_GSC0NtbI1ozRBA1w1Vp9cpfp974syN',
@@ -28,6 +28,12 @@ def load_data():
     }
 
     historical_data = {country: pd.read_csv(file) for country, file in historical_files.items()}
+    
+    # Ajout de la colonne 'region' vide pour les pays sans région
+    for country in historical_data.keys():
+        if country not in ['FR', 'US']:
+            historical_data[country]['region'] = pd.NA
+    
     df = pd.read_csv('data/prepared_data.csv', parse_dates=['date 1ere commande (Restaurant)', 'Date de commande'], decimal='.')
     
     to_exclude_commande = ['CANCELLED', 'ABANDONED', 'FAILED', 'WAITING']
@@ -36,6 +42,10 @@ def load_data():
     df = df[~df['Statut paiement'].isin(to_exclude_paiement)]
     df = df[~df['Canal'].str.contains('trading', case=False, na=False)]
     df['Mois'] = df['Date de commande'].dt.strftime('%Y-%m')
+    
+    # Ajout de la colonne 'region' vide pour le DataFrame principal
+    if 'region' not in df.columns:
+        df['region'] = pd.NA
     
     return historical_data, df
 
