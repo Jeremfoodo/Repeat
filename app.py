91ba5f3c2d48784f3b8fa936155367c8acd68b5f
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-07-22 13:42:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 from global_analysis import global_analysis
 from account_analysis import account_analysis
 from objectifs import objectifs_page
@@ -9,17 +10,21 @@ from src.data_processing import load_data, download_files, reassign_account_mana
 download_files()
 historical_data, df = load_data()
 
-# Menu horizontal
-tab1, tab2, tab3, tab4 = st.tabs(["Analyse Globale", "Par Account", "Objectifs", "Active Users"])
+# Menu vertical
+selected = option_menu(
+    "Menu",
+    ["Analyse Globale", "Par Account", "Objectifs", "Active Users"],
+    icons=["graph-up", "person", "bullseye", "bar-chart"],
+    menu_icon="cast",
+    default_index=0,
+    orientation="vertical",
+)
 
-with tab1:
+if selected == "Analyse Globale":
     global_analysis(historical_data, df)
-
-with tab2:
+elif selected == "Par Account":
     account_analysis(df)
-
-with tab3:
+elif selected == "Objectifs":
     objectifs_page(historical_data, df)
-
-with tab4:
+elif selected == "Active Users":
     active_users_page(historical_data, df)
