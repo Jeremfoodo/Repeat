884af4e48cad662fa998d97d7f419c9bf89ff40f
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-07-22 18:21:18 +0200

    Update data_processing.py

--- src/data_processing.py
+++ src/data_processing.py
@@ -3,16 +3,20 @@ import gdown
 import os
 import streamlit as st
 
-# URL de Google Drive pour le fichier volumineux
+# URL de Google Drive pour les fichiers
 prepared_data_url = 'https://drive.google.com/uc?id=1krOrcWcYr2F_shA4gUYZ1AQFsuWja9dM'
+objectifs_url = 'https://drive.google.com/uc?id=1UaAh3PUaHjBTShTYUGkd-Yw6fUT7TZJN'
 
 @st.cache_data
 def download_prepared_data():
     data_dir = 'data'
     os.makedirs(data_dir, exist_ok=True)
     
-    output = os.path.join(data_dir, 'prepared_data.csv')
-    gdown.download(prepared_data_url, output, quiet=False)
+    output_prepared = os.path.join(data_dir, 'prepared_data.csv')
+    output_objectifs = os.path.join(data_dir, 'objectifs.xlsx')
+    
+    gdown.download(prepared_data_url, output_prepared, quiet=False)
+    gdown.download(objectifs_url, output_objectifs, quiet=False)
 
 @st.cache_data
 def load_data():
@@ -35,6 +39,12 @@ def load_data():
     df['Mois'] = df['Date de commande'].dt.strftime('%Y-%m')
     
     return historical_data, df
+
+@st.cache_data
+def load_objectifs():
+    data_dir = 'data'
+    objectifs_df = pd.read_excel(os.path.join(data_dir, 'objectifs.xlsx'), engine='openpyxl')
+    return objectifs_df
 
 def reassign_account_manager(df):
     df = df.sort_values(by=['Restaurant ID', 'Date de commande'])
