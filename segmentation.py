796eb15a8fc10124bbecc8d685e55bdf9f27855b
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-07-23 13:55:19 +0200

    Update segmentation.py

--- segmentation.py
+++ segmentation.py
@@ -13,12 +13,26 @@ def get_clients_by_segment_and_spending(df, target_month):
     labels = ['Basic', 'Silver', 'Gold', 'High Spenders']
     target_orders['Spending Level'] = pd.cut(target_orders.groupby('Restaurant ID')['Total'].transform('sum'), bins=bins, labels=labels)
     
+    # Définir les segments
+    previous_month = (pd.to_datetime(target_month) - pd.DateOffset(months=1)).strftime('%Y-%m')
+    acquisition = target_orders[target_orders['date 1ere commande (Restaurant)'].dt.strftime('%Y-%m') == target_month]
+    nouveaux_clients = target_orders[target_orders['date 1ere commande (Restaurant)'].dt.strftime('%Y-%m') == previous_month]
+    clients_recents = target_orders[target_orders['date 1ere commande (Restaurant)'].dt.strftime('%Y-%m').isin(
+        [(pd.to_datetime(target_month) - pd.DateOffset(months=i)).strftime('%Y-%m') for i in range(2, 6)]
+    )]
+    anciens_clients = target_orders[target_orders['date 1ere commande (Restaurant)'].dt.strftime('%Y-%m') < (pd.to_datetime(target_month) - pd.DateOffset(months=5)).strftime('%Y-%m')]
+    
+    target_orders.loc[acquisition.index, 'Segment'] = 'Acquisition'
+    target_orders.loc[nouveaux_clients.index, 'Segment'] = 'Nouveaux Clients'
+    target_orders.loc[clients_recents.index, 'Segment'] = 'Clients Récents'
+    target_orders.loc[anciens_clients.index, 'Segment'] = 'Anciens Clients'
+    
     # Compter les clients et les dépenses totales par segment et niveau de dépense
     heatmap_data = target_orders.groupby(['Segment', 'Spending Level']).agg({'Restaurant ID': 'nunique', 'Total': 'sum'}).reset_index()
     
     # Pivot pour obtenir le format désiré
-    heatmap_pivot = heatmap_data.pivot(index='Segment', columns='Spending Level', values='Restaurant ID')
-    spending_pivot = heatmap_data.pivot(index='Segment', columns='Spending Level', values='Total')
+    heatmap_pivot = heatmap_data.pivot(index='Segment', columns='Spending Level', values='Restaurant ID').fillna(0)
+    spending_pivot = heatmap_data.pivot(index='Segment', columns='Spending Level', values='Total').fillna(0)
     
     return heatmap_pivot, spending_pivot
 
@@ -43,7 +57,10 @@ def segmentation_page(df):
             x=heatmap_data_june.columns,
             y=heatmap_data_june.index,
             colorscale='Viridis',
-            hoverongaps=False))
+            hoverongaps=False,
+            text=heatmap_data_june.values,
+            texttemplate="%{text}"
+        ))
         fig.update_layout(
             title='Nombre de Clients par Segment et Niveau de Dépense',
             xaxis_title='Niveau de Dépense',
@@ -57,7 +74,10 @@ def segmentation_page(df):
             x=spending_data_june.columns,
             y=spending_data_june.index,
             colorscale='Viridis',
-            hoverongaps=False))
+            hoverongaps=False,
+            text=spending_data_june.values,
+            texttemplate="%{text}"
+        ))
         fig_spending.update_layout(
             title='Dépenses Totales par Segment et Niveau de Dépense',
             xaxis_title='Niveau de Dépense',
@@ -72,7 +92,10 @@ def segmentation_page(df):
             x=heatmap_data_july.columns,
             y=heatmap_data_july.index,
             colorscale='Viridis',
-            hoverongaps=False))
+            hoverongaps=False,
+            text=heatmap_data_july.values,
+            texttemplate="%{text}"
+        ))
         fig.update_layout(
             title='Nombre de Clients par Segment et Niveau de Dépense',
             xaxis_title='Niveau de Dépense',
@@ -86,7 +109,10 @@ def segmentation_page(df):
             x=spending_data_july.columns,
             y=spending_data_july.index,
             colorscale='Viridis',
-            hoverongaps=False))
+            hoverongaps=False,
+            text=spending_data_july.values,
+            texttemplate="%{text}"
+        ))
         fig_spending.update_layout(
             title='Dépenses Totales par Segment et Niveau de Dépense',
             xaxis_title='Niveau de Dépense',
