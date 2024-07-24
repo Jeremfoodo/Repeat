4027b91926fca15592e6e87dfec58a0c21bd694b
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-07-24 17:52:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
 import streamlit as st
-from src.data_processing import load_data, download_prepared_data, load_recent_purchases
-from segmentation import segmentation_page
+import pandas as pd
 from client_info import client_info_page
+from segmentation import segmentation_page
+from src.data_processing import load_data, load_recent_purchases
 
 # Charger les données
 df = load_data()
 df_recent_purchases = load_recent_purchases()
 
-# Définir les pages
-selected = st.sidebar.selectbox("Sélectionner une page", ["Segmentation", "Client Info"])
+# Extraire les paramètres de requête
+query_params = st.experimental_get_query_params()
+selected_client_id = query_params.get('client_id', [None])[0]
 
-# Afficher la page correspondante
+# Sélectionner la page
+selected = st.sidebar.selectbox("Menu", ["Segmentation", "Client Info"])
+
 if selected == "Segmentation":
     segmentation_page(df)
 elif selected == "Client Info":
-    query_params = st.experimental_get_query_params()
-    if 'client_id' in query_params:
-        client_id = int(query_params['client_id'][0])
-        client_info_page(df, df_recent_purchases, client_id)
+    if selected_client_id:
+        client_info_page(df, df_recent_purchases, int(selected_client_id))
     else:
-        st.write("Veuillez sélectionner un client depuis la page Segmentation.")
+        st.write("Aucun client sélectionné.")
