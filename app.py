d28d09347fd58bf39726fc76350146e25aa089bc
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-07-24 18:12:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
 import streamlit as st
-import pandas as pd
-from client_info import client_info_page
+from src.data_processing import load_data, load_recent_purchases, load_objectifs
 from segmentation import segmentation_page
-from src.data_processing import load_data, load_recent_purchases
+from client_info import client_info_page
 
 # Charger les données
-df = load_data()
+historical_data, df = load_data()
 df_recent_purchases = load_recent_purchases()
+objectifs_df = load_objectifs()
 
 # Extraire les paramètres de requête
 query_params = st.experimental_get_query_params()
-selected_client_id = query_params.get('client_id', [None])[0]
+page = query_params.get("page", ["segmentation"])[0]
+client_id = query_params.get("client_id", [None])[0]
 
-# Sélectionner la page
-selected = st.sidebar.selectbox("Menu", ["Segmentation", "Client Info"])
-
-if selected == "Segmentation":
+# Afficher la page correspondante
+if page == "segmentation":
     segmentation_page(df)
-elif selected == "Client Info":
-    if selected_client_id:
-        client_info_page(df, df_recent_purchases, int(selected_client_id))
-    else:
-        st.write("Aucun client sélectionné.")
+elif page == "client_info" and client_id is not None:
+    client_info_page(df, df_recent_purchases, int(client_id))
+else:
+    st.write("Page non trouvée")
