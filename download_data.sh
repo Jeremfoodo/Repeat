d7a0f810ecb7fd53f7aef761bb271445c62bd220
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-07-25 14:52:05 +0200

    Create download_data.sh

---
+++ download_data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Download the segmentation data
+gdown "https://docs.google.com/spreadsheets/d/1lCVTDYtM_SWj1W5OqTr15-56K4BCWPqf/edit?usp=sharing&ouid=115682724849456076163&rtpof=true&sd=true" -O segmentation_data.xlsx
