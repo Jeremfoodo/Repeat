4df3f868f773ca697f9685bebff66f332bcee87f
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-07-25 14:59:49 +0200

    Update segmentation.py

--- src/segmentation.py
+++ src/segmentation.py
@@ -1,3 +1,6 @@
+import gdown
+import pandas as pd
+
 def load_segmentation_data():
     url = 'https://drive.google.com/uc?id=1lCVTDYtM_SWj1W5OqTr15-56K4BCWPqf'
     output = 'segmentation_data.xlsx'
