6aa2fddeca3f1d962f9a6339e78f49dc6ba190d8
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-07-25 14:58:21 +0200

    Update segmentation.py

--- src/segmentation.py
+++ src/segmentation.py
@@ -1,4 +1,6 @@
-import pandas as pd
-
-def load_segmentation_data(file_path="segmentation_data.xlsx"):
-    return pd.read_excel(file_path, engine='openpyxl')
+def load_segmentation_data():
+    url = 'https://drive.google.com/uc?id=1lCVTDYtM_SWj1W5OqTr15-56K4BCWPqf'
+    output = 'segmentation_data.xlsx'
+    gdown.download(url, output, quiet=False)
+    df = pd.read_excel(output, engine='openpyxl')
+    return df
